1246abbfc97af71922b6dfbce33a00cd5dc3b783
Malelion <gert3987@student.eadania.dk>
2019-03-11 06:37:38 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+<script>  
+
+function loadJord() {
+  location.replace("jord.php")
+}            
+function loadCo2() {
+  location.replace("co2.php")
+}
+function loadTemp() {
+  location.replace("temp.php")
+}
+function loadHum() {
+  location.replace("hum.php")
+}
+function loadHome() {
+  location.replace("icons.php")
+}
+
+</script>
